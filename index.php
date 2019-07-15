fb11c426c8f09bee03db824497158c3be429651f
angavatar25 <51564299+angavatar25@users.noreply.github.com>
2019-07-15 17:26:50 +0700

    Added some script

--- index.php
+++ index.php
@@ -7,26 +7,36 @@
 </head>
 <body>
    <?php include($prefix."include/tracking-script/body.php"); ?>
-   <div class="hamburger" onclick="openNav()" id="nav">
-      <div class="hamburger__container">
-         <div class="hamburger__inner"></div>
-         <div class="hamburger__hidden"></div>
-      </div>
-      <div class="hamburger-content">
-         <ul class="navbar-content">
-            <li><a href="#">About</a></li>
-            <li><a href="#">People</a></li>
-            <li><a href="#">Perspective</a></li>
-            <li><a href="#">Companies</a></li>
-            <li><a href="#">More</a></li>
-         </ul>
-      </div>
+   <div class="hamburger" id="burger">
+      <div class="garis"></div>
+      <div class="garis"></div>
+      <div class="garis"></div>
+      <div class="garis"></div>
    </div>
-
+   <div class="hamburger-content" id="shownav">
+      <ul class="navbar-content">
+         <li><a href="#">About</a></li>
+         <li><a href="#">People</a></li>
+         <li><a href="#">Perspective</a></li>
+         <li><a href="#">Companies</a></li>
+         <li><a href="#">More</a></li>
+      </ul>
+   </div>
 </body>
+<?php include($prefix."include/script.php"); ?>
 <script>
-   document.querySelector(".hamburger").addEventListener("click", e => {
-      e.currentTarget.classList.toggle("is-active");
-   });
+   // document.querySelector(".hamburger").addEventListener("click", e => {
+   //    e.currentTarget.classList.toggle("is-active");
+   // });
+
+   function openNav() {
+      document.getElementById("shownav").style.display = 'block'
+   }
+
+   $("#burger").click(function(){
+      $(".hamburger-content").toggleClass("active");
+      $(".garis").toggleClass("active");
+      // $(".garis").css("background", "white");
+   })
 </script>
 </html>
